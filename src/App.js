9b47e7213bf74286fba686cf030722b0d01832af
tanyushanel <tatyana.nelipovich@gmail.com>
2024-04-18 19:10:40 +0200

    feat: slider markup added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header/Header";
 import { Intro } from "./components/Intro/Intro";
+import { Slider } from "./components/Slider/Slider";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Intro />
+      <Slider />
     </div>
   );
 }
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,58 @@
+import style from "./Slider.module.scss";
+
+import cx from "classnames";
+
+export const Slider = () => {
+  return (
+    <article id="slider" className={cx(style.sliderContainer, "center")}>
+      <h3>
+        Choose your <span className="accent-txt">favorite</span> coffee
+      </h3>
+      <div className={cx(style.slider, "flex-center  margin-40")}>
+        <button
+          className={cx(
+            style.sliderButton,
+            "flex-center previous button-secondary"
+          )}
+        >
+          <span className="material-symbols-outlined"> arrow_back </span>
+        </button>
+        <div className={style.sliderContent}>
+          <div className={style.slide}>
+            <img
+              className={style.sliderImg}
+              src="assets/pics/coffee-slider-1.png"
+              alt="pic"
+            />
+            <h4 className="margin-15">S’mores Frappuccino</h4>
+            <p className="medium-txt margin-15">
+              This new drink takes an espresso and mixes it with brown sugar and
+              cinnamon before being topped with oat milk.
+            </p>
+            <h4 className="price">$5.50</h4>
+          </div>
+        </div>
+        <button
+          className={cx(
+            style.sliderButton,
+            style.next,
+            "flex-center button-secondary"
+          )}
+        >
+          <span className="material-symbols-outlined"> arrow_forward </span>
+        </button>
+      </div>
+      <div className={cx(style.sliderControl, "flex-center gap-10")}>
+        <p className={cx(style.sliderControl, style.sliderControl.active)}>
+          <span></span>
+        </p>
+        <p className={style.sliderControl}>
+          <span></span>
+        </p>
+        <p className={style.sliderControl}>
+          <span></span>
+        </p>
+      </div>
+    </article>
+  );
+};
