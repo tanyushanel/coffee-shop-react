9c35304be64837d4b58547d2ad711be33786a5f9
tanyushanel <tatyana.nelipovich@gmail.com>
2024-04-19 11:38:58 +0200

    feat: added footer markup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Intro } from "./components/Intro/Intro";
 import { Slider } from "./components/Slider/Slider";
 import { About } from "./components/About/About";
 import { Mobile } from "./components/Mobile/Mobile";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Slider />
       <About />
       <Mobile />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,67 @@
+import style from "./Footer.module.scss";
+
+import cx from "classnames";
+
+export const Footer = () => {
+  return (
+    <footer
+      className={cx(
+        style.footer,
+        "flex-wrap bg-fill-dark light-color margin-100"
+      )}
+    >
+      <article id="contact" className={style.contactHeading}>
+        <h3 className="light-color">
+          Sip, Savor, Smile.
+          <span className="accent-txt">It’s coffee time! </span>
+        </h3>
+        <div className={cx("flex social-button-container margin-40 gap-12")}>
+          <button
+            className={cx(style.socialButton, "flex-center button-secondary")}
+          >
+            <img src="assets/icons/tweet.svg" alt="tweet-pic" />
+          </button>
+          <button
+            className={cx(style.socialButton, "flex-center button-secondary")}
+          >
+            <img src="assets/icons/instagram.svg" alt="inst-pic" />
+          </button>
+          <button
+            className={cx(style.socialButton, "flex-center button-secondary")}
+          >
+            <img src="assets/icons/fb.svg" alt="fb-pic" />
+          </button>
+        </div>
+      </article>
+
+      <div className={style.contactsDesc}>
+        <h4 className="margin-30">Contact us</h4>
+
+        <ul className="margin-40">
+          <li className="flex margin-20 gap-10">
+            <img src="assets/icons/location.svg" alt="loc-icon" />
+            <a
+              className="medium-txt"
+              href="https://www.google.com/maps/search/8558+Green+Rd.,+LA"
+              target="blank"
+            >
+              8558 Green Rd., LA
+            </a>
+          </li>
+
+          <li className="flex margin-20 gap-10">
+            <img src="assets/icons/phone-icon.svg" alt="phone-icon" />
+            <a className="medium-txt" href="tel:1(603)555-0123">
+              +1 (603) 555-0123
+            </a>
+          </li>
+
+          <li className="flex margin-20 gap-10">
+            <img src="assets/icons/clock.svg" alt="clock-icon" />
+            <span className="medium-txt">Mon-Sat: 9:00 AM – 23:00 PM</span>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
